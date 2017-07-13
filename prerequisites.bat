13ada992d7d4937e5ce08f5f17c071149c9eef52
dtitco69 <daryl.titcomb@swarthoull.com>
2017-07-13 13:56:23 +0100

    RDP and autologin
    added RDP, auto login of ICR user, disable windows defender, and add ICR to RDP group

--- prerequisites.bat
+++ prerequisites.bat
@@ -19,10 +19,6 @@ EXIT /B 1
 echo Only continue if you are sure
 pause
 
-::Check and add ICR user
-net user | find /i "ICR" || Net user ICR Acce$$2020 /add /fullname:"Tempgp"
-net localgroup administrators ICR /add
-
 ::Stop and disable windows firewall
 net stop MpsSvc
 REG add "HKLM\SYSTEM\CurrentControlSet\services\MpsSvc" /v Start /t REG_DWORD /d 4 /f
@@ -32,6 +28,28 @@ REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v Hide
 
 ::disable UAC
 REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
+
+::disable Windows Defender
+REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
+REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableRoutinelyTakingAction /t REG_DWORD /d 1 /f
+
+::Enable RDP
+reg add "hklm\system\currentControlSet\Control\Terminal Server" /v "AllowTSConnections" /t REG_DWORD /d 1 /f
+reg add "hklm\system\currentControlSet\Control\Terminal Server" /v "fDenyTSConnections" /t REG_DWORD /d 0 /f
+sc config TermService start= auto
+net start Termservice
+
+::Check and add ICR user
+net user | find /i "ICR" || Net user ICR Acce$$2020 /add /fullname:"Tempgp"
+net localgroup administrators ICR /add
+
+::Set ICR to auto login
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 1 /f
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /t REG_SZ /d ICR /f
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d Acce$$2020 /f
+
+::Add ICR to RDP group
+NET LOCALGROUP "Remote Desktop Users" ICR /ADD
 
 echo Press 'Enter' To restart your PC
 pause
